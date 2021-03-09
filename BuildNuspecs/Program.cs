c872809c2fc9f5de716e6558d22f36a61214f3b8
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-03-09 18:13:03 +0000

    Changed name of repo/solution to ModMon.Books

---
+++ BuildNuspecs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BuildNuspecs
+{
+    //Once you have build the NuSpecs you run then with the following commands 
+    // DEBUG:   dotnet pack -p:NuspecFile=CreateNuGetDebug.nuspec
+    // RELEASE: dotnet pack -c Release -p:NuspecFile=CreateNuGetRelease.nuspec
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
