c9057f9834f8143752c4f44f68d935fba5e1e0bd
Jon P Smith <jon.smith@selectiveanalytics.com>
2021-03-11 18:56:58 +0000

    Traverse projects and builds Nuspec

---
+++ BuildNuspecs/ParseProjects/ProjectsParser.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) 2021 Jon P Smith, GitHub: JonPSmith, web: http://www.thereformedprogrammer.net/
+// Licensed under MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace BuildNuspecs.ParseProjects
+{
+    public static class ProjectsParser
+    {
+        public static AppStructureInfo ParseModularMonolithApp(this string solutionDir, string rootName)
+        {
+            var projFilePaths = Directory.GetDirectories(solutionDir)
+                    .Where(dir => Path.GetFileNameWithoutExtension(dir).Contains($"{rootName}"))
+                    .SelectMany(dir =>
+                Directory.GetFiles(dir, "*.csproj")).ToList();
+            var pInfo = projFilePaths
+                .Select(path => new ProjectInfo(path))
+                .ToDictionary(x => x.ProjectName);
+            //Now we look at each csproj in turn and fill in
+            //- What target framework it has (used to build Nuspec)
+            //- What NuGet packages it uses (used to build Nuspec)
+            //- What it links to (used in displaying links)
+            foreach (var projFilePath in projFilePaths)
+            {
+                var filename = Path.GetFileNameWithoutExtension(projFilePath);
+                var projectDecoded = DeserializeToObject<Project>(projFilePath);
+
+                var projectToUpdate = pInfo[filename];
+
+                //get target framework
+                projectToUpdate.TargetFramework = projectDecoded.PropertyGroup.TargetFramework;
+
+                //get NuGet packages
+
+                projectToUpdate.NuGetPackages = projectDecoded.ItemGroup
+                    ?.SingleOrDefault(x => x?.PackageReference?.Any() == true)
+                    ?.PackageReference
+                    .Select(x => new NuGetInfo(x))
+                    .ToList() ?? new List<NuGetInfo>();
+
+                // Fill in references to other packages
+                projectToUpdate.ChildProjects = projectDecoded.ItemGroup
+                    ?.SingleOrDefault(x => x?.ProjectReference?.Any() == true)
+                    ?.ProjectReference
+                    .Select(x => pInfo[Path.GetFileNameWithoutExtension(x.Include)])
+                    .ToList() ?? new List<ProjectInfo>();
+            }
+
+            return new AppStructureInfo(rootName, pInfo);
+        }
+
+        private static T DeserializeToObject<T>(this string filepath) where T : class
+        {
+            System.Xml.Serialization.XmlSerializer ser = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using StreamReader sr = new StreamReader(filepath);
+            return (T)ser.Deserialize(sr);
+        }
+    }
+}
--- BuildNuspecs/Program.cs
+++ BuildNuspecs/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using BuildNuspecs.Helpers;
+using BuildNuspecs.NuspecBuilder;
+using BuildNuspecs.ParseProjects;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace BuildNuspecs
 {
@@ -8,9 +16,51 @@ namespace BuildNuspecs
 
     class Program
     {
+        private static IConfigurationRoot _configurationRoot;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //see https://docs.microsoft.com/en-us/dotnet/core/extensions/configuration#configure-console-apps
+            using var host = CreateHostBuilder(args).Build();
+
+            var thisProjPath = ProjectHelpers.GetExecutingAssemblyPath();
+            var solutionDir = thisProjPath.GetSolutionPathFromProjectPath();
+
+            var debugMode = CalcDebugMode(args);
+
+            var settings = new Settings(_configurationRoot, debugMode, solutionDir);
+
+            var rootName = solutionDir.GetSolutionFilename();
+            var appStructure = solutionDir.ParseModularMonolithApp(rootName);
+            settings.BuildNuspecFile(appStructure);
         }
+        //see https://docs.microsoft.com/en-us/dotnet/core/extensions/configuration-providers#json-configuration-provider
+        static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                //thanks to https://wakeupandcode.com/generic-host-builder-in-asp-net-core-3-1/
+                .ConfigureHostConfiguration(configuration =>
+                {
+                    configuration
+                        .AddJsonFile("hostsettings.json", optional: true);
+
+                    _configurationRoot = configuration.Build();
+                });
+
+        private static bool CalcDebugMode(string[] args)
+        {
+            var debugMode = false;
+#if DEBUG
+            debugMode = true;
+#endif
+            //If there is a argument, then this overrides
+            if (args.Length > 0)
+            {
+                if (args.Length != 1 || !(args[0] == "R" || args[0] == "D"))
+                    throw new Exception("If you provide an argument it must be R (for Release) or D (for Debug)");
+                debugMode = args[0] == "D";
+            }
+            return debugMode;
+        }
+
     }
 }
